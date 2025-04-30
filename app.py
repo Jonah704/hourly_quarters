a207ddf134fb2e2ca5f7122e2102b7cd61730815
TuckerArrants <68343441+TuckerArrants@users.noreply.github.com>
2025-04-30 13:26:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def load_quartal(urls, version="v2"):   # <- bump this string to invalidate
 
 # 2) Call the loader for 1H and 3H data
 url_1h_eq   = "https://raw.githubusercontent.com/TuckerArrants/hourly_quarters/main/ES_NQ_YM_Hourly_Quartal_1min_Processed_from_2016.csv"
-url_1h_comm = "https://raw.githubusercontent.com/TuckerArrants/hourly_quarters/main/CL_GC_Hourly_Quartal_1min_Processed_from_2016.csv"
+url_1h_comm = "https://raw.githubusercontent.com/TuckerArrants/hourly_quarters/main/CL_GC_NG_SI_Hourly_Quartal_1min_Processed_from_2016.csv"
 url_3h_eq   = "https://raw.githubusercontent.com/TuckerArrants/hourly_quarters/main/ES_NQ_YM_3H_Quartal_1min_Processed_from_2016.csv"
 url_3h_comm = "https://raw.githubusercontent.com/TuckerArrants/hourly_quarters/main/CL_GC_3H_Quartal_1min_Processed_from_2016.csv"
 
